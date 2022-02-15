e92fd27369758bc325ecdbd110fb07ff475b700c
Hugo Payen <hageval@outlook.fr>
2022-02-15 13:22:09 +0100

    JS

--- js/main.js
+++ js/main.js
@@ -6,3 +6,15 @@ function triggerOff() {
 
 /* Date Auto */
 new Date().getFullYear();document.getElementById("year").innerHTML = new Date().getFullYear();
+
+/* Age */
+<script type="text/javascript">
+    function getAge(date) { 
+        var diff = Date.now() - date.getTime();
+        var age = new Date(diff); 
+
+        return Math.abs(age.getUTCFullYear() - 1970);
+    }
+
+    alert(getAge(new Date(1995, 12, 6))); //Date(année, mois, jour)   
+</script>
